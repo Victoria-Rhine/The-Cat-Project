e291c82cda569eb18a86e8f4886a41c703ea351d
Victoria-Rhine <vrhine18@mail.wou.edu>
2019-12-18 17:21:19 -0800

    Add buttons on ptags details

--- TheCatProject/Controllers/PTagsController.cs
+++ TheCatProject/Controllers/PTagsController.cs
@@ -23,6 +23,9 @@ namespace TheCatProject.Controllers
                 .Select(m => new PersonalityDetailsView { CatID = m.cpt.c.ID, CatName = m.cpt.c.Name, 
                 pTagID = m.cpt.pt.ID, PersonalityID = m.p.ID, PersonalityType = m.p.Type }).ToList();
 
+            // i think....
+            ViewBag.TraitID = ptag.ID;
+
             return View(personalityModel);
         }
 
