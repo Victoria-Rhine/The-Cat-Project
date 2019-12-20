75ca648114bdf1c58d2093c632e6b33f059908db
Victoria-Rhine <vrhine18@mail.wou.edu>
2019-12-19 17:31:06 -0800

    Add information pages

--- TheCatProject/Controllers/InformationController.cs
+++ TheCatProject/Controllers/InformationController.cs
@@ -6,29 +6,24 @@ using System.Web.Mvc;
 
 namespace TheCatProject.Controllers
 {
-    public class MiscController : Controller
+    public class InformationController : Controller
     {
-        // GET: Misc
+        // GET: Information
         public ActionResult Index()
         {
             return View();
         }
 
+        // page to display information about the website
         public ActionResult About()
         {
             return View();
         }
 
-        public ActionResult BreedInfo()
+        // page to link websites to assist user input
+        public ActionResult BeforeYouStart()
         {
             return View();
         }
-
-        public ActionResult PatternInfo()
-        {
-            return View();
-        }
-
-
     }
 }
