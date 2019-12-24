35006020be36ba8f78812c12bf4991ef4c050922
Victoria-Rhine <vrhine18@mail.wou.edu>
2019-12-24 11:53:06 -0800

    Add summary page and placeholders for nav bar

--- TheCatProject/Controllers/InformationController.cs
+++ TheCatProject/Controllers/InformationController.cs
@@ -1,17 +1,48 @@
 ﻿using System.Web.Mvc;
+using TheCatProject.DAL;
 
 namespace TheCatProject.Controllers
 {
     public class InformationController : Controller
     {
+        private CatsContext db = new CatsContext();
         // page to display information about the website
         public ActionResult About()
         {
             return View();
         }
 
-        // page to link websites to assist user input
-        public ActionResult BeforeYouStart()
+        public ActionResult Guide()
+        {
+            return View();
+        }
+
+        public ActionResult Stats()
+        {
+            return View();
+        }
+
+        public ActionResult Breeds()
+        {
+            return View();
+        }
+
+        public ActionResult Colors()
+        {
+            return View();
+        }
+
+        public ActionResult Names()
+        {
+            return View();
+        }
+
+        public ActionResult Personalities()
+        {
+            return View();
+        }
+
+        public ActionResult Summary()
         {
             return View();
         }
--- TheCatProject/Scripts/main.js
+++ TheCatProject/Scripts/main.js
@@ -4,3 +4,9 @@
         "CHANGE DETAILS to edit" + "<br />" + "CONTINUE to add traits" + "<br />" + "ALL DONE to quit process"
 }
 
+function pageDetailsEnd() {
+    document.getElementById("details").innerHTML =
+        "<br />" + "Let's make sure things look right" + "<br /><br />" +
+        "CHANGE DETAILS to edit" + "<br />" + "FINISH AND VIEW to see summary"
+}
+
